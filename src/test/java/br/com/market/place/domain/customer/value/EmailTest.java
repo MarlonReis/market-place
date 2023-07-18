bd081148ed7ebd016017f8a0d5622b56a5816149
Marlon Reis <marlon-reis91@hotmail.com>
2023-07-18 11:23:11 -0300

    test: ensure that email work as expected

--- src/test/java/br/com/market/place/domain/customer/value/EmailTest.java
+++ src/test/java/br/com/market/place/domain/customer/value/EmailTest.java
@@ -1,4 +1,40 @@
+package br.com.market.place.domain.customer.value;
+
+import br.com.market.place.domain.shared.exception.InvalidDataException;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmailTest {
-  
+    @Test
+    void shouldReturnValidEmail() {
+        Email email = new Email("any@email.com");
+        assertThat(email.email(), Matchers.is("any@email.com"));
+    }
+
+    @NullSource
+    @ParameterizedTest
+    @ValueSource(strings = {""})
+    void shouldInvalidDataExceptionWhenReceiveNullOrEmpty(String email) {
+        var exception = assertThrows(InvalidDataException.class, () -> new Email(email));
+        assertThat(exception.getMessage(), Matchers.is("Attribute email is required!"));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"any", "any.com", "any com", "@com.br"})
+    void shouldInvalidDataExceptionWhenReceiveInvalidEmail(String email) {
+        var exception = assertThrows(InvalidDataException.class, () -> new Email(email));
+        assertThat(exception.getMessage(), Matchers.is("Attribute email is invalid!"));
+    }
+
+    @Test
+    void shouldReturnNullWhenNotSpecifyArgument() {
+        assertThat(new Email().email(), Matchers.nullValue());
+    }
 }
