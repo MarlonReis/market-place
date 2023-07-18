50fb756fbe085b2fbc911af944c3fa7333a3f595
Marlon Reis <marlon-reis91@hotmail.com>
2023-07-18 10:39:17 -0300

    test: ensure that name work as expected

---
+++ src/test/java/br/com/market/place/domain/customer/value/NameTest.java
@@ -0,0 +1,29 @@
+package br.com.market.place.domain.customer.value;
+
+import br.com.market.place.domain.shared.exception.InvalidDataException;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NameTest {
+    @Test
+    void shouldReturnFullNameWhenSetCorrectParams() {
+        Name name = new Name("Michael Jackson");
+        assertThat(name.name(), Matchers.is("Michael Jackson"));
+    }
+
+    @Test
+    void shouldThrowInvalidDataExceptionWhenReceiveNull() {
+        var exception = assertThrows(InvalidDataException.class, () -> new Name(null));
+        assertThat(exception.getMessage(),Matchers.is("Attribute name is required!"));
+    }
+
+    @Test
+    void shouldThrowInvalidDataExceptionWhenReceiveEmpty() {
+        var exception = assertThrows(InvalidDataException.class, () -> new Name(""));
+        assertThat(exception.getMessage(),Matchers.is("Attribute name is required!"));
+    }
+
+}
