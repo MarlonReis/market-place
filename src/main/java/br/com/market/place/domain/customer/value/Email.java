64fdcb48387a85f9affc3e48ad2bbeb0aa5a4666
Marlon Reis <marlon-reis91@hotmail.com>
2023-07-18 11:17:57 -0300

    feat: create email value object

---
+++ src/main/java/br/com/market/place/domain/customer/value/Email.java
@@ -0,0 +1,24 @@
+package br.com.market.place.domain.customer.value;
+
+import br.com.market.place.domain.shared.exception.InvalidDataException;
+
+import static am.ik.yavi.builder.StringValidatorBuilder.of;
+
+public class Email {
+    private String email;
+
+    public Email(String name) {
+        var nameVal = of("email", s -> s.notBlank()
+                .message("Attribute email is required!")
+                .email().message("Attribute email is invalid!")
+        ).build().validate(name);
+        this.email = nameVal.orElseThrow(InvalidDataException::new);
+    }
+
+    protected Email() {
+    }
+
+    public String email() {
+        return this.email;
+    }
+}
