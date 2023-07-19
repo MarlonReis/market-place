e10a7e559c282d783008b5ed02ad1d408e88b782
Marlon Reis <marlon-reis91@hotmail.com>
2023-07-18 23:40:23 -0300

    feat: create domain date value object

--- src/main/java/br/com/market/place/domain/shared/value/DomainDate.java
+++ src/main/java/br/com/market/place/domain/shared/value/DomainDate.java
@@ -1,2 +1,34 @@
-package br.com.market.place.domain.shared.value;public class DomainDete {
+package br.com.market.place.domain.shared.value;
+
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class DomainDate {
+    private LocalDateTime date;
+
+    protected DomainDate(){
+        date = LocalDateTime.now();
+    }
+
+    public final LocalDateTime date(){
+        return date;
+    }
+
+    public final String dateFormatted(){
+        return date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (o instanceof DomainDate that){
+            return Objects.equals(date,that.date());
+        }
+        return false;
+    }
+
 }
