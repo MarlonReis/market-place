56253672efdd89ca7cade1aa19b399ad05f6988e
Marlon Reis <marlon-reis91@hotmail.com>
2023-07-19 11:29:44 -0300

    feat: create update at value object

---
+++ src/main/java/br/com/market/place/domain/shared/value/UpdateAt.java
@@ -0,0 +1,21 @@
+package br.com.market.place.domain.shared.value;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.time.LocalDateTime;
+
+@Embeddable
+public class UpdateAt extends DomainDate {
+    public UpdateAt() {
+        super();
+    }
+
+    @Override
+    @AttributeOverrides({@AttributeOverride(name = "date", column = @Column(name = "updateAt"))})
+    public LocalDateTime getDate() {
+        return super.getDate();
+    }
+}
