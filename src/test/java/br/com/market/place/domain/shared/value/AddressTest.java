5ecb0b68db2d37d2bdb1ffd3fde84e24d11307c7
Marlon Reis <marlon-reis91@hotmail.com>
2023-07-19 15:34:23 -0300

    test: ensure that address value object work as expected

--- src/test/java/br/com/market/place/domain/shared/value/AddressTest.java
+++ src/test/java/br/com/market/place/domain/shared/value/AddressTest.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.market.place.domain.shared.value;
+
+import br.com.market.place.domain.shared.exception.InvalidDataException;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class AddressTest {
-  
+    @Test
+    void shouldReturnValidAddress() {
+        Address address = new Address("London", "Baker Street", "221", "B", "37540-232");
+
+        assertThat(address.city(), Matchers.is("London"));
+        assertThat(address.street(), Matchers.is("Baker Street"));
+        assertThat(address.number(), Matchers.is("221"));
+        assertThat(address.component(), Matchers.is("B"));
+        assertThat(address.zipCode(), Matchers.is("37540232"));
+
+    }
+
+    @Test
+    void shouldThrowsInvalidDataExceptionWhenReceiveInvalidField() {
+        var exception = assertThrows(InvalidDataException.class, () -> new Address(
+                "London",
+                "Baker Street",
+                "221",
+                "",
+                "375423000")
+        );
+        assertThat(exception.getMessage(),Matchers.is("Attribute component is required!"));
+    }
+    @Test
+    void shouldReturnNullWhenUseDefaultConstructor(){
+        Address address = new Address();
+
+        assertThat(address.city(), Matchers.nullValue());
+        assertThat(address.street(), Matchers.nullValue());
+        assertThat(address.number(), Matchers.nullValue());
+        assertThat(address.component(), Matchers.nullValue());
+        assertThat(address.zipCode(), Matchers.nullValue());
+    }
+
 }
